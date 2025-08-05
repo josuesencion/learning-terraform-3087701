ee1205bdfaa5296bbf80471cd91861d31d6af72a
josuesencion <43467896+josuesencion@users.noreply.github.com>
2025-08-05 01:29:12 -0400

    fix autoscaling error

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "blog_vpc" {
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.1"
+  version = "6.5.2"
   
   name = "blog"
   min_size = 1
@@ -43,7 +43,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-  security_group      = [module.blog_sg.security_group_id] 
+  security_groups     = [module.blog_sg.security_group_id] 
 
   image_id               = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
